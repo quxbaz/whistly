204ceecbd5c1d912126f0a4d226603dd845ff923
David Yeung <quxbaz@gmail.com>
2014-03-31 00:04:05 -0400

    Added find files function.

--- main.py
+++ main.py
@@ -12,5 +12,17 @@ app.config.from_object(conf)
 def index():
   return render_template('index.html')
 
+# Walks through root directories and compiles a list of file names.
+def find_js_files(roots):
+  if isinstance(roots, basestring):
+    roots = [roots]
+  js_files = []
+  for root in roots:
+    for (dirname, dirnames, filenames) in walk(root):
+      js_files += [path.join(dirname, filename) for filename in filenames if isScript(filename)]
+  print js_files
+  return js_files
+
+
 if __name__ == '__main__':
   app.run(debug=True)
