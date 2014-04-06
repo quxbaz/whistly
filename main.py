9a9bcc1b2ae891daabc5966317228e45a25985c3
David Yeung <quxbaz@gmail.com>
2014-04-06 01:46:34 -0400

    Generalized isScript function

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ app.config.from_object(conf)
 
 @app.route('/')
 def index():
-  scripts = find_js_files('static/js/app')
+  scripts = find_files('static/js/app', '.js')
   return render_template('index.html', scripts=scripts)
 
-def isScript(script):
-  return path.splitext(script)[1] == '.js'
+def is_type(filename, filetype):
+  return path.splitext(filename)[1] == filetype
 
 # Walks through root directories and compiles a list of file names.
-def find_js_files(roots):
+def find_files(roots, filetype):
   if isinstance(roots, basestring):
     roots = [roots]
   js_files = []
   for root in roots:
     for (dirname, dirnames, filenames) in walk(root):
-      js_files += [path.join(dirname, filename) for filename in filenames if isScript(filename)]
+      js_files += [path.join(dirname, filename) for filename in filenames if is_type(filename, filetype)]
   return js_files
 
 if __name__ == '__main__':
