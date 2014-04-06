e8632a2b98b9d46c233e6c09ceb49f080b45d0ad
David Yeung <quxbaz@gmail.com>
2014-04-05 22:03:22 -0400

    Now auto-finding scripts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask
 from flask import Flask, render_template, request, url_for, send_file
 from conf import conf
+from os import path, walk
 # import random
 # import uuid
 # import json
@@ -10,7 +11,11 @@ app.config.from_object(conf)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  scripts = find_js_files('static/js/app')
+  return render_template('index.html', scripts=scripts)
+
+def isScript(script):
+  return path.splitext(script)[1] == '.js'
 
 # Walks through root directories and compiles a list of file names.
 def find_js_files(roots):
@@ -20,9 +25,7 @@ def find_js_files(roots):
   for root in roots:
     for (dirname, dirnames, filenames) in walk(root):
       js_files += [path.join(dirname, filename) for filename in filenames if isScript(filename)]
-  print js_files
   return js_files
-
 
 if __name__ == '__main__':
   app.run(debug=True)
