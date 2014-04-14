20c2a0a42f69a65694b05e4e3f37896d26afd154
David Yeung <quxbaz@gmail.com>
2014-04-13 22:15:04 -0400

    Added text-editable component.

--- static/js/app/com/component.js
+++ static/js/app/com/component.js
@@ -10,4 +10,16 @@ define('com/component', function(App, mixin) {
   App.FocusInputComponent = Em.TextField.extend(mixin.el.Focus);
   App.FocusTextareaComponent = Em.TextArea.extend(mixin.el.Focus);
 
+  App.TextEditableComponent = Em.Component.extend({
+    layout: Em.Handlebars.compile('{{value}}'),
+    classNames: ['text-editable'],
+    attributeBindings: ['contentEditable'],
+    contentEditable: 'true',
+    didInsertElement: function() {
+      this._super.apply(this, arguments);
+      if (this.get('focus'))
+        this.$().focus();
+    }
+  });
+
 });
