3eab3c3dd7283d39a225b00779bfb71cd9b79f8b
David Yeung <quxbaz@gmail.com>
2014-04-16 00:36:49 -0400

    Added try-else helper

--- static/js/app/com/helper.js
+++ static/js/app/com/helper.js
@@ -7,9 +7,20 @@
 
 define('com/helper', function() {
 
-  Em.Handlebars.helper('format-lines', function(value) {
+  var helper = function() {
+    return Em.Handlebars.helper.apply(Em.Handlebars, arguments);
+  };
+
+  helper('format-lines', function(value) {
     var escaped = Em.Handlebars.Utils.escapeExpression(value);
     return new Em.Handlebars.SafeString(_.trim(escaped).replace(/\n/gm, '<br />'));
+  });
+
+  helper('try-else', function(value, elseValue) {
+    if (value)
+      return value;
+    else
+      return elseValue;
   });
 
 });
